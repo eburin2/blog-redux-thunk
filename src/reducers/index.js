cc187b54c588d23d366e0f6ad60f2bf37f3d5054
Elliot Burin <eburin2@gmail.com>
2019-08-15 11:29:49 -0700

    fix switch statement errors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,4 @@
 import { combineReducers } from 'redux';
 import postsReducer from './postsReducer';
 
-export default combineReducers({
-  posts: postsRuducer
-});
+export default combineReducers({ posts: postsReducer });
